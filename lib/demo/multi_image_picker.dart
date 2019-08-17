d5137b7f379b37ea46072a68a38622ea544bd6bb
meng <714405544@qq.com>
2019-08-17 15:01:25 +0800

    从Mac提交代码 多图选择库无法运行，暂时关掉

--- lib/demo/multi_image_picker.dart
+++ lib/demo/multi_image_picker.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:multi_image_picker/multi_image_picker.dart';
+//import 'package:multi_image_picker/multi_image_picker.dart';
 
 /// 作者 mcy
 /// 时间 2019/8/13 17:46
@@ -13,7 +13,7 @@ class MultiImagePickerPage extends StatefulWidget {
 }
 
 class _MultiImagePickerPageState extends State<MultiImagePickerPage> {
-  List<Asset> images = List<Asset>();
+//  List<Asset> images = List<Asset>();
 
   @override
   Widget build(BuildContext context) {
@@ -25,60 +25,60 @@ class _MultiImagePickerPageState extends State<MultiImagePickerPage> {
       body: Column(
         children: <Widget>[
           OutlineButton(
-            onPressed: loadAssets,
+//            onPressed: loadAssets,
             child: Text("选择图片"),
           ),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 3,
-              children: List.generate(images.length, (index) {
-                Asset a = images[index];
-                return AssetThumb(asset: a, width: 300, height: 300);
-              }),
-            ),
-          )
+//          Expanded(
+//            child: GridView.count(
+//              crossAxisCount: 3,
+//              children: List.generate(images.length, (index) {
+//                Asset a = images[index];
+//                return AssetThumb(asset: a, width: 300, height: 300);
+//              }),
+//            ),
+//          )
         ],
       ),
     );
   }
 
-  Future<void> loadAssets() async {
-    List<Asset> resultList = List<Asset>();
-    String error = 'No Error Dectected';
-
-    try {
-      resultList = await MultiImagePicker.pickImages(
-        maxImages: 9,
-        selectedAssets: images,
-        materialOptions: MaterialOptions(
-          actionBarTitle: "图片",
-          allViewTitle: "选择图片",
-          actionBarColor: "#C91B3A",
-          actionBarTitleColor: "#ffffff",
-          lightStatusBar: false,
-          statusBarColor: '#C91B3A',
-          startInAllView: true,
-          selectCircleStrokeColor: "#000000",
-          selectionLimitReachedText: "你不能再选择了。",
-          textOnNothingSelected: "你还没有选图片。"
-        ),
-        cupertinoOptions: CupertinoOptions(
-          selectionFillColor: "#ff11ab",
-          selectionTextColor: "#ff00a5",
-          selectionCharacter: "✓",
-        ),
-      );
-    } on Exception catch (e) {
-      error = "";
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      images = resultList;
-    });
-  }
+//  Future<void> loadAssets() async {
+//    List<Asset> resultList = List<Asset>();
+//    String error = 'No Error Dectected';
+//
+//    try {
+//      resultList = await MultiImagePicker.pickImages(
+//        maxImages: 9,
+//        selectedAssets: images,
+//        materialOptions: MaterialOptions(
+//          actionBarTitle: "图片",
+//          allViewTitle: "选择图片",
+//          actionBarColor: "#C91B3A",
+//          actionBarTitleColor: "#ffffff",
+//          lightStatusBar: false,
+//          statusBarColor: '#C91B3A',
+//          startInAllView: true,
+//          selectCircleStrokeColor: "#000000",
+//          selectionLimitReachedText: "你不能再选择了。",
+//          textOnNothingSelected: "你还没有选图片。"
+//        ),
+//        cupertinoOptions: CupertinoOptions(
+//          selectionFillColor: "#ff11ab",
+//          selectionTextColor: "#ff00a5",
+//          selectionCharacter: "✓",
+//        ),
+//      );
+//    } on Exception catch (e) {
+//      error = "";
+//    }
+//
+//    // If the widget was removed from the tree while the asynchronous platform
+//    // message was in flight, we want to discard the reply rather than calling
+//    // setState to update our non-existent appearance.
+//    if (!mounted) return;
+//
+//    setState(() {
+//      images = resultList;
+//    });
+//  }
 }
